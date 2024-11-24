d1ab4d0e64ee2a2cf58c1fce1e8e0b2550071388
MaestroFidel <160391061+MaestroFidel@users.noreply.github.com>
2024-11-24 16:51:42 +0200

    header-rew

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -5,16 +5,19 @@ const menuLinks = document.querySelectorAll('.js-menu__link');
 
 hamburgerButton.addEventListener('click', () => {
   mobileMenu.classList.add('active');
+  document.body.classList.add('no-scroll')
 });
 
 closeButton.addEventListener('click', () => {
   mobileMenu.classList.remove('active');
+  document.body.classList.remove('no-scroll');
 });
 
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
       mobileMenu.classList.remove('active');
       const targetId = link.getAttribute('href').substring(1); 
-      document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+
+      
     });
   });
