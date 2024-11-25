76350b4f89cdbf70b603853c99be2dbb40ced5a4
Oksana <oksanavasilevna77@gmail.com>
2024-11-25 10:39:12 +0200

    change dark theme method init

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,21 +1,31 @@
 import { getDataLocalStorage, setDataToLocalStorage } from './localStorage-api';
-
 const checkbox = document.querySelector('#switch');
-const themeLink = document.querySelector('#theme-link');
 const KEY_SWITCH = 'theme';
 
-
-toggleTheme();
-
 checkbox.addEventListener('click', onClick);
+// Create link to prevent vite wrong links order
+createLink();
 
+const themeLink = document.querySelector('#theme-link');
 function toggleTheme() {
   const theme = getDataLocalStorage(KEY_SWITCH);
-  themeLink.href = theme === 'dark' ? './css/dark-theme.css' : '';
+  themeLink.href =
+    theme === 'dark'
+      ? new URL('../css/dark-theme.css', import.meta.url).href
+      : '';
   checkbox.checked = theme === 'dark';
 }
+
+toggleTheme();
 
 function onClick(event) {
   setDataToLocalStorage(KEY_SWITCH, event.target.checked ? 'dark' : '');
   toggleTheme();
+}
+
+function createLink() {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.id = 'theme-link';
+  document.head.append(link);
 }
